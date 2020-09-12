bfa304d1727240d0d14853a5a39f7168a93b488f
Wil Thieme <wil.thieme@protonmail.com>
2020-09-12 15:14:13 -0400

    add mood lighting option
    toggle night mode between fire or true night

--- scripts/automoodlighting.lua
+++ scripts/automoodlighting.lua
@@ -6,11 +6,22 @@ function onInit()
 	if User.isHost() then
 		DB.addHandler('calendar.current.hour', 'onUpdate', onTimeChanged)
 	end
+	
+	OptionsManager.registerOption2('AUTONIGHTMODE', false, 'option_header_game', 'opt_lab_automood_nightcolor', 'option_entry_cycler', 
+		{ labels = 'opt_val_automood_nightcolor_fire', values = 'fire', baselabel = 'opt_val_automood_nightcolor_night'})
 end
 
 function onTimeChanged(node)
 	local nodeCurrentTime = node.getParent()
 	local nHour = DB.getValue(nodeCurrentTime, 'hour')
-	if nHour < 6 or nHour >= 20 then Interface.setLighting('FFFFFFFF', 'FFFFB8A4', 'FFFFFFFF', 'FFFFFFFF') -- night
-	elseif nHour >= 6 and nHour < 20 then Interface.setLighting('FFFFFFFF', 'FFFFFFFF', 'FFFFFFFF', 'FFFFFFFF') end -- day
+	
+	if nHour < 6 or nHour >= 20 then
+		if OptionsManager.isOption('AUTONIGHTMODE', 'fire') then
+			Interface.setLighting('FFFFFFFF', 'FFB1C6F4', 'FFFFFFFF', 'FFFFFFFF') -- campfire
+		else
+			Interface.setLighting('FFFFFFFF', 'FFFFB8A4', 'FFFFFFFF', 'FFFFFFFF') -- night
+		end
+	elseif nHour >= 6 and nHour < 20 then
+		Interface.setLighting('FFFFFFFF', 'FFFFFFFF', 'FFFFFFFF', 'FFFFFFFF') -- day
+	end
 end
