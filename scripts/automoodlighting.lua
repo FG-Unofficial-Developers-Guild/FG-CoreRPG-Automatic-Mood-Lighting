85a963991021f40cdcfa3923f7e6a7abc0a3306b
Wil Thieme <wil.thieme@protonmail.com>
2020-10-03 22:56:21 -0400

    avoid nil error

--- scripts/automoodlighting.lua
+++ scripts/automoodlighting.lua
@@ -3,26 +3,25 @@
 --
 
 function onInit()
-	if User.isHost() then
-		DB.addHandler('calendar.current.hour', 'onUpdate', onTimeChanged)
-	end
-	
 	OptionsManager.registerOption2('AUTONIGHTMODE', false, 'option_header_game', 'opt_lab_automood_nightcolor', 'option_entry_cycler', 
 		{ labels = 'opt_val_automood_nightcolor_fire', values = 'fire', baselabel = 'opt_val_automood_nightcolor_night'})
 	
-	onTimeChanged()
+	if User.isHost() then
+		DB.addHandler('calendar.current.hour', 'onUpdate', onTimeChanged)
+		onTimeChanged()
+	end
 end
 
 function onTimeChanged()
 	local nHour = DB.getValue(DB.findNode('calendar.current'), 'hour')
 	
-	if nHour < 6 or nHour >= 20 then
+	if nHour and ((nHour < 6) or (nHour >= 20)) then
 		if OptionsManager.isOption('AUTONIGHTMODE', 'fire') then
 			Interface.setLighting('FFFFFFFF', 'FFB1C6F4', 'FFFFFFFF', 'FFFFFFFF') -- campfire
 		else
 			Interface.setLighting('FFFFFFFF', 'FFFFB8A4', 'FFFFFFFF', 'FFFFFFFF') -- night
 		end
-	elseif nHour >= 6 and nHour < 20 then
+	elseif nHour and ((nHour >= 6) and (nHour < 20)) then
 		Interface.setLighting('FFFFFFFF', 'FFFFFFFF', 'FFFFFFFF', 'FFFFFFFF') -- day
 	end
 end
